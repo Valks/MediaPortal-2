feb16ebab4593cc2133761e440e8301a988bc4d7
Valks <wayne@yrpnetworks.com.au>
2013-05-22 19:47:10 +1000

    Disabled Microsoft GeoLocation Service due to security prompt breaking the user experience. Added NoData to City.cs Implemented fix for UI Updating when all locations removed not triggering in WeatherModel.cs and CurrentWeatherModel.cs (note: needs to be updated with dev for a few patches to work properly). Removed duplicate code in Weather/Models/WeatherModel.cs and weather.xaml regarding first time detection. Updated Weather/Models/WeatherSetupModel.cs to reset properly when no locations are stored.

--- MediaPortal/Source/UI/UiComponents/Weather/Models/CurrentWeatherModel.cs
+++ MediaPortal/Source/UI/UiComponents/Weather/Models/CurrentWeatherModel.cs
@@ -20,10 +20,8 @@
     along with MediaPortal 2. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#endregion
+#endregion Copyright (C) 2007-2013 Team MediaPortal
 
-using System;
-using System.Threading;
 using MediaPortal.Common;
 using MediaPortal.Common.General;
 using MediaPortal.Common.Localization;
@@ -33,6 +31,8 @@ using MediaPortal.Common.Settings;
 using MediaPortal.Common.Threading;
 using MediaPortal.UI.Presentation.Models;
 using MediaPortal.UiComponents.Weather.Settings;
+using System;
+using System.Threading;
 
 namespace MediaPortal.UiComponents.Weather.Models
 {
@@ -45,7 +45,8 @@ namespace MediaPortal.UiComponents.Weather.Models
     /// Update interval for current location.
     /// </summary>
     private const int WEATHER_UPDATE_INTERVAL = 30 * 60 * 1000;
-    protected readonly AbstractProperty _currentLocationProperty = new WProperty(typeof(City), new City("No Data", "No Data"));
+
+    protected readonly AbstractProperty _currentLocationProperty = new WProperty(typeof(City), City.NoData);
 
     /// <summary>
     /// Exposes the current location to the skin.
@@ -60,28 +61,29 @@ namespace MediaPortal.UiComponents.Weather.Models
     /// </summary>
     public City CurrentLocation
     {
-      get { return (City) _currentLocationProperty.GetValue(); }
+      get { return (City)_currentLocationProperty.GetValue(); }
       set { _currentLocationProperty.SetValue(value); }
     }
 
     /// <summary>
-    /// Contructs a new instance of <see cref="CurrentWeatherModel"/>. 
+    /// Contructs a new instance of <see cref="CurrentWeatherModel"/>.
     /// </summary>
-    public CurrentWeatherModel() : base(true, WEATHER_UPDATE_INTERVAL)
+    public CurrentWeatherModel()
+      : base(true, WEATHER_UPDATE_INTERVAL)
     {
       // do initial update in its own thread to avoid delay during startup of MP2
       ServiceRegistration.Get<IThreadPool>().Add(new DoWorkHandler(this.SetAndUpdatePreferredLocation), "SetAndUpdatePreferredLocation", QueuePriority.Normal, ThreadPriority.BelowNormal);
       SubscribeToMessages();
     }
 
-    void SubscribeToMessages()
+    private void SubscribeToMessages()
     {
       _messageQueue.SubscribeToMessageChannel(WeatherMessaging.CHANNEL);
       _messageQueue.SubscribeToMessageChannel(LocalizationMessaging.CHANNEL);
       _messageQueue.MessageReceived += OnMessageReceived;
     }
 
-    void OnMessageReceived(AsynchronousMessageQueue queue, SystemMessage message)
+    private void OnMessageReceived(AsynchronousMessageQueue queue, SystemMessage message)
     {
       if (message.ChannelName == WeatherMessaging.CHANNEL && ((WeatherMessaging.MessageType)message.MessageType) == WeatherMessaging.MessageType.LocationChanged ||
           message.ChannelName == LocalizationMessaging.CHANNEL && ((LocalizationMessaging.MessageType)message.MessageType) == LocalizationMessaging.MessageType.LanguageChanged)
@@ -93,16 +95,19 @@ namespace MediaPortal.UiComponents.Weather.Models
       // do update in its own thread to avoid delay
       ServiceRegistration.Get<IThreadPool>().Add(new DoWorkHandler(SetAndUpdatePreferredLocation), "SetAndUpdatePreferredLocation", QueuePriority.Normal, ThreadPriority.BelowNormal);
     }
-    
+
     protected void SetAndUpdatePreferredLocation()
     {
+      _currentLocationProperty.SetValue(City.NoData);
+
       WeatherSettings settings = ServiceRegistration.Get<ISettingsManager>().Load<WeatherSettings>();
-      if (settings == null || settings.LocationsList == null)
+      if (settings.LocationsList == null)
         return;
 
       CitySetupInfo city = settings.LocationsList.Find(loc => loc.Id == settings.LocationCode);
       if (city == null)
         return;
+
       bool result = false;
       try
       {
